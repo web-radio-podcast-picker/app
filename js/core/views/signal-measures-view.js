e6d9d1725c34e78da517786a63edeb1f5dd56925
franck gaspoz <franck.gaspoz@gmail.com>
2025-06-27 16:17:30 +0200

    add bottom pane

--- js/core/views/signal-measures-view.js
+++ js/core/views/signal-measures-view.js
@@ -15,7 +15,7 @@ class SignalMeasuresView {
         $('#iv_' + c).text(this.signalMeasures.volts);
         $('#vmin_' + c).text(this.signalMeasures.vMin);
         $('#vmax_' + c).text(this.signalMeasures.vMax);
-        $('#vavg_' + c).text(this.signalMeasures.vAvg);
+        $('#vavg_' + c).text(this.signalMeasures.vAvg.toFixed(7));
 
         $('#stime').text(oscilloscope.scanPeriod);
         $('#sfrq').text(oscilloscope.scanFrq);
