e50ac9d8bc520e6144c5e1919802b9e1d07a07ca
franck gaspoz <franck.gaspoz@gmail.com>
2025-09-04 16:34:49 +0200

    impl wave to level

--- js/core/views/gauge-view.js
+++ js/core/views/gauge-view.js
@@ -19,26 +19,66 @@ class GaugeView {
 
     run() {
 
-        const dataArray = this.channel?.getSamplesTask?.fftDataArray;
+        const minDb = -130
+        const fc = 0.5
 
-        if (dataArray != null) {
+        //this.fftToLevels(this.channel?.getSamplesTask?.fftDataArray, minDb, fc)
+        this.waveToLevels(this.channel?.getSamplesTask?.dataArray, minDb, fc)
+    }
 
-            var value = dataArray[0];       // -1 .. 1
+    waveToLevels(dataArray, minDb, fc) {
+        if (dataArray == null) return
 
-            var sum = 0
-            for (var i = 0; i < dataArray.length; i += 1) {
-                sum += dataArray[i]
-            }
-            var v = sum / dataArray.length
-            const minDb = -130
-            // limit to 0..minDb
-            v = Math.max(v, minDb)
-            if (v > 0) v = 0
-
-            // 0..r-1     r..2*r-1      2*r .. 3*r-1    (#plots intervals)
-            this.tplots(v, minDb, this.$leftGauge)
-            this.tplots(v, minDb, this.$rightGauge)
+        var sum = 0
+        const n = dataArray.length
+        for (var i = 0; i < dataArray.length; i += 1) {
+            sum += Math.abs(dataArray[i])
         }
+        var v = sum / dataArray.length      // 0..1
+        v *= this.plots
+        v = Math.min(v, this.plots)
+        this.wplots(v, this.$leftGauge)
+        this.wplots(v, this.$rightGauge)
+
+    }
+
+    wplots(v, t) {
+        var i = this.plots - 1
+        const r = 1 / this.plots
+        var cv = 0
+
+        while (i >= 0) {
+            const e = t[i]
+            const $e = $(e)
+            if (v > cv) {
+                // plot on
+                $e.addClass('gauge-plot-on')
+            } else {
+                // plot off
+                $e.removeClass('gauge-plot-on')
+            }
+            cv += r
+            i--
+        }
+    }
+
+    fftToLevels(dataArray, minDb, fc) {
+        if (dataArray == null) return
+
+        var value = dataArray[0];       // -1 .. 1
+
+        var sum = 0
+        for (var i = 0; i < dataArray.length; i += 1) {
+            sum += dataArray[i]
+        }
+        var v = sum / dataArray.length * fc
+        // limit to 0..minDb
+        v = Math.max(v, minDb)
+        if (v > 0) v = 0
+
+        // 0..r-1     r..2*r-1      2*r .. 3*r-1    (#plots intervals)
+        this.tplots(v, minDb, this.$leftGauge)
+        this.tplots(v, minDb, this.$rightGauge)
     }
 
     tplots(v, minDb, t) {
