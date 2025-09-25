f1977c956d5d91282436b7b76a18cfe8c6e7f979
franck gaspoz <franck.gaspoz@gmail.com>
2025-09-25 15:19:29 +0200

    force app mode through manifest

--- js/core/settings.js
+++ js/core/settings.js
@@ -14,7 +14,7 @@ window.settings = {
             verDate: '8/23/2025'
         },
         wrp: {
-            version: '1.2.15.1',
+            version: '1.2.15.2',
             verDate: '9/25/2025'
         }
     },
