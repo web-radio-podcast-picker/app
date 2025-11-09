29a77d0c09757252989ff8518bb287afea376f27
franck gaspoz <franck.gaspoz@gmail.com>
2025-11-09 16:00:14 +0100

    fix items layouts (progress bar,height,text wrap)

--- js/core/settings.js
+++ js/core/settings.js
@@ -14,7 +14,7 @@ window.settings = {
             verDate: '8/23/2025'
         },
         wrp: {
-            version: '1.3.11.2',
+            version: '1.3.11.3',
             verDate: '11/09/2025'
         }
     },
