e97682b5c05f86c3978793e5533f26792e9b9c94
franck gaspoz <franck.gaspoz@gmail.com>
2025-11-14 19:23:16 +0100

    restruct item layout fix chromium center text

--- js/core/settings.js
+++ js/core/settings.js
@@ -14,7 +14,7 @@ window.settings = {
             verDate: '8/23/2025'
         },
         wrp: {
-            version: '1.3.33',  // prep 33
+            version: '1.3.34',  // prep 34
             verDate: '11/14/2025'
         }
     },
--- js/modules/web-radio-picker/components/ui/builders/rad-list-builder.js
+++ js/modules/web-radio-picker/components/ui/builders/rad-list-builder.js
@@ -45,9 +45,12 @@ class RadListBuilder {
             xcl += 'wrp-list-item-text-container-pdc'
             str = '<img name="pod" src="./img/icons8-podcast-50.png" class="wrp-item-pod-icon">' + str
         }
-        const $textBox = $('<div class="wrp-list-item-text-container ' + xcl + '">'
-            + '<div class="wrp-list-item-text-title">' + str + '</div>'   // TODO: encapsulate in a div + style in grid + fix code that update this
-            + '</div>')
+        const $textBox = $('<div class="wrp-list-item-text-container ' + xcl + '"></div>')
+        const $textBoxSubContainer = $('<div class="wrp-list-item-text-subcontainer"></div>')
+
+        const $textBoxText = $('<div class="wrp-list-item-text-title">' + str + '</div>')   // TODO: encapsulate in a div + style in grid + fix code that update this
+
+        $textBoxSubContainer.append($textBoxText)
 
         // TODO: FIX :: listId == listName == null on startup ?!
         // eventually sub text
@@ -67,7 +70,8 @@ class RadListBuilder {
             if (rdItem.subText2 && rdItem.subText2 != '')
                 $subt2.text(rdItem.subText2)
 
-            $textBox.append($subTextBox)
+            //$textBox.append($subTextBox)
+            $textBoxSubContainer.append($subTextBox)
 
             if (rdItem.subText && rdItem.subText != '')
                 $subt1.removeClass('hidden')
@@ -80,6 +84,7 @@ class RadListBuilder {
                 $subt2.addClass('hidden')
         }
 
+        $textBox.append($textBoxSubContainer)
         $item.append($textBox)
 
         // eventually fav icon
